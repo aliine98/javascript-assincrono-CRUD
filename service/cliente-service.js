e0b84d357f9116aa200d82c2714542f1f45e10e3
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2023-08-22 23:02:14 -0300

    🐛 fix: typo in url from client-service.js

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,75 +1,71 @@
 const listaClientes = () => {
-   return fetch('http://localhost:3000/profle')
-      .then(resposta => {
-         if (resposta.ok) {
+    return fetch("http://localhost:3000/profile").then((resposta) => {
+        if (resposta.ok) {
             return resposta.json();
-         }
-         throw new Error('Não foi possível listar os clientes.');
-      });
-}
+        }
+        throw new Error("Não foi possível listar os clientes.");
+    });
+};
 
 const criaCliente = (nome, email) => {
-   return fetch('http://localhost:3000/profile', {
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-         nome: nome,
-         email: email
-      })
-   })
-      .then(resposta => {
-         if (resposta.ok) {
+    return fetch("http://localhost:3000/profile", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email,
+        }),
+    }).then((resposta) => {
+        if (resposta.ok) {
             return resposta.body;
-         }
-         throw new Error('Não foi possível criar um cliente.');
-      });
-}
+        }
+        throw new Error("Não foi possível criar um cliente.");
+    });
+};
 
 const removeCliente = (id) => {
-   return fetch(`http://localhost:3000/profile/${id}`, {
-      method: 'DELETE',
-   }).then(resposta => {
-      if (!resposta.ok) {
-         throw new Error('Não foi possível excluir o cliente.');
-      }
-   });
-}
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: "DELETE",
+    }).then((resposta) => {
+        if (!resposta.ok) {
+            throw new Error("Não foi possível excluir o cliente.");
+        }
+    });
+};
 
 const detalhaCliente = (id) => {
-   return fetch(`http://localhost:3000/profile/${id}`)
-      .then(resposta => {
-         if (resposta.ok) {
+    return fetch(`http://localhost:3000/profile/${id}`).then((resposta) => {
+        if (resposta.ok) {
             return resposta.json();
-         }
-         throw new Error('Não foi possível detalhar o cliente.');
-      });
-}
+        }
+        throw new Error("Não foi possível detalhar o cliente.");
+    });
+};
 
 const atualizaCliente = (id, nome, email) => {
-   return fetch(`http://localhost:3000/profile/${id}`, {
-      method: 'PUT',
-      headers: {
-         'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-         nome: nome,
-         email: email
-      })
-   })
-      .then(resposta => {
-         if (resposta.ok) {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email,
+        }),
+    }).then((resposta) => {
+        if (resposta.ok) {
             return resposta.json();
-         }
-         throw new Error('Não foi possível atualizar o cliente.');
-      });
-}
+        }
+        throw new Error("Não foi possível atualizar o cliente.");
+    });
+};
 
 export const clienteService = {
-   listaClientes,
-   criaCliente,
-   removeCliente,
-   detalhaCliente,
-   atualizaCliente
-}
+    listaClientes,
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente,
+};
